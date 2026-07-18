e15fdc7f5162f28720b681a253839d79eebd1edd
Bhawna Singh <bhawnasingh8928@gmail.com>
2026-07-18 10:41:16 +0530

    Added Strings

---
+++ Largest.java
@@ -0,0 +1,23 @@
+class Largest {
+
+    public static String largestString(String fruits[]){
+
+        String largest = fruits[0];
+
+        for(int i=1; i<fruits.length; i++){
+
+         if ((largest.compareTo(fruits[i])) < 0){
+            largest = fruits[i];
+         }
+        }
+        return largest;
+    }
+    
+
+
+    public static void main(String args[]){
+        String fruits[] = {"apple","banana","orange"};
+
+        System.out.println(largestString(fruits));
+    }
+}
